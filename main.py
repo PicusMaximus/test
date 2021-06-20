eee54211cf4aeacb0fc2329885a6865638bf158f
Picus Maximus <86190811+PicusMaximus@users.noreply.github.com>
2021-06-20 21:48:26 +0200

    test

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
+import discord
+import os
+from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('Discord_Token')
+
+client = commands.Bot(command_prefix= '!')
+
+@client.event
+async def on_ready():
+    print('we are ready')
+
+client.run(TOKEN)
+
+
+
 
